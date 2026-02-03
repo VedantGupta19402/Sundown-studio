1268c4c3e45d2cb0e51cde2d56cdccb3d2d0ac2f
VedantGupta19402 <vedantguptav@gmail.com>
2026-02-03 12:00:00 +0530

    Merged project updates for Feb 3

---
+++ web.js
@@ -0,0 +1,24 @@
+// const locomotiveScroll = new LocomotiveScroll({
+//   el: document.querySelector('.main'),
+//   smooth: true
+// });
+
+
+
+let ki = document.querySelector(".elements-container");
+let fix = document.querySelector("#fixed-img")
+ki.addEventListener("mouseenter", () => {
+    fix.style.display = "block";
+})
+
+ki.addEventListener("mouseleave", () => {
+    fix.style.display = "none";
+})
+
+let elem = document.querySelectorAll(".elements");
+elem.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
+        let image = e.getAttribute("data-image")
+        fix.style.backgroundImage = `url(${image})`
+    })
+})

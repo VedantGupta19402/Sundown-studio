05cb89b593119f14a7c33beefd4a4bde96f0ffba
VedantGupta19402 <vedantguptav@gmail.com>
2026-02-05 23:29:37 +0530

    tenth commit

--- web.js
+++ web.js
@@ -22,3 +22,13 @@ elem.forEach((e) => {
         fix.style.backgroundImage = `url(${image})`
     })
 })
+
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
